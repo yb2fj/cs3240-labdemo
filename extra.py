f99d2c5bd7277d3e65099d2441fd23917f02383b
Yuri Bang <yb2fj@virginia.edu>
2015-02-16 16:18:12 -0500

    adding extra.py

--- extra.py
+++ extra.py
@@ -1,11 +1,11 @@
 __author__ = 'yb2fj'
 
+from helper import appreciation
 
-from helper import greeting
 
 def main():
     msg = "whoa"
-    greeting(msg)
+    appreciation(msg)
 
 if __name__=='__main__':
     main()
--- helper.py
+++ helper.py
@@ -4,9 +4,15 @@ __author__ = 'yb2fj'
 def greeting(msg):
     print(msg)
 
+
+def appreciation(msg):
+    print(msg + ", Thank you")
+
+
 def test_greeting():
-    msg = "hello"
+    msg = "Thank you"
     greeting(msg)
+
 
 def main():
     test_greeting()
